261e3291bb3fdf35856eea1998c21c87ef299328
wqgk <1227077096@qq.com>
2016-04-11 20:20:57 +0800

    添加遥控器丢失的标志

--- App/inc/aid_control.h
+++ App/inc/aid_control.h
@@ -1,5 +1,7 @@
 #ifndef __AID_CONTROL_H
 #define __AID_CONTROL_H
 
+void Aid_Control_Led_Alarm(void);
+void Aid_Control_Crash(void);
 
 #endif
--- App/src/aid_control.c
+++ App/src/aid_control.c
@@ -10,17 +10,17 @@ void Aid_Control_Led_Alarm(void)
 
 	LED_Control.event = Event_Ready;
 
-	if(1)
+	if(0)
 		LED_Control.event = Event_Calibration;
-	if(1)
+	if(lost_remote_flag)
 		LED_Control.event = Event_Lost_Remote;
-	if(1)
+	if(0)
 		LED_Control.event = Event_Calibration_Fail;
 	if(Battery.Battery_Alarm)
 		LED_Control.event = Event_Low_Voltage;
 	if(imu_calibrate_flag)
 		LED_Control.event = Event_Calibration;
-	if(1)
+	if(0)
 		LED_Control.event = Event_Auto_Land;
 	if(Battery.Battery_Charge_State)
 		LED_Control.event = Event_Batter_Charge;
